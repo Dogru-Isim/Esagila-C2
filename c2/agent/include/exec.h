24893da532df89b1f40b1df37d14ea61490a1d12
mint <n.sogutlu@student.fontys.nl>
2024-12-21 12:48:58 +0100

    move exec.h to src/modules/include

--- c2/agent/include/exec.h
+++
@@ -1,5 +0,0 @@
-#ifndef EXEC_H
-#define EXEC_H
-
-
-#endif  // EXEC_H
---
+++ c2/agent/src/modules/include/exec.h
@@ -0,0 +1,8 @@
+#ifndef EXEC_H
+#define EXEC_H
+
+#include <windows.h>
+
+VOID injectRawShellcode(PVOID shellcode);
+
+#endif  // EXEC_H
