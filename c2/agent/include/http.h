c044501d0a9635e988006479b6db3b3b8b316f55
mint <n.sogutlu@student.fontys.nl>
2025-01-12 15:25:21 +0100

    add docstrings to http.h

--- c2/agent/include/http.h
+++ c2/agent/include/http.h
@@ -9,10 +9,41 @@
 #define WINHTTP_HEADER_NAME_BY_INDEX NULL
 #define WINHTTP_NO_HEADER_INDEX NULL
 
+/*
+This function sends a get request to a web server
+
+Input:
+    [in] PAPI api: a pointer to the API structure
+    [in] WCHAR* wcServer: host name or an IP address of an HTTP server
+    [in] INTERNET_PORT port: port number
+    [in] WCHAR* wcPath: path to query
+Output:
+    Success -> CHAR*: web server response, needs to be freed
+    Failure -> NULL
+*/
 CHAR* GetRequest(PAPI api, WCHAR* wcServer, INTERNET_PORT port, WCHAR* wcPath);
 
+/*
+This function sends a post request to an endpoint
+
+Input:
+    [in] PAPI api: a pointer to the API struct
+    [in] WCHAR* server: a hostname or an IP address
+    [in] INTERNET_PORT port: a port number
+    [in] const WCHAR endpoint: a path on the server
+    [in] CCHAR* data: a json data
+Output:
+    The function returns nothing
+*/
 void PostRequest(PAPI api, WCHAR* server, INTERNET_PORT port, const WCHAR* endpoint, CHAR* data);
 
+//
+/*
+the return need to be freed
+
+Note:
+    TODO: combine this function with GetRequest
+*/
 LPVOID httpGetExecutable(PAPI api, PDWORD pdwDllSize, WCHAR* wEndpoint, WCHAR* wcServer, INTERNET_PORT dwPort);
 
 #endif  // HTTP_H
