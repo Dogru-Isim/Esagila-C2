f141fc196d2c1ca5a0c5a14625ee8eb12ff6a7a9
mint <n.sogutlu@student.fontys.nl>
2025-01-13 12:38:58 +0100

    add explanatory comments

--- c2/agent/src/std.c
+++ c2/agent/src/std.c
@@ -5,9 +5,12 @@
  *
  * Input:
  *      const char *p1: first string
+ *
  *      const char *p1: second string
+ *
  * Output:
  *      0 -> strings are the same
+ *
  *      Non-zero value -> strings are different
  */
 int my_strcmp(const char *p1, const char *p2)
@@ -31,6 +34,7 @@ int my_strcmp(const char *p1, const char *p2)
  *
  * Input:
  *      size_t len: the size of the base64 encoded value
+ *
  * Output:
  *      size_t -> size of the base64 decoded value
  */
@@ -48,15 +52,47 @@ size_t base64_raw_size(size_t len)
     return (len * 3) / 4 - padding;
 }
 
-void * myMemcpy (void *dest, const void *src, size_t len)
+/*
+ * This function copies a region in memory onto another
+ * 
+ * Input:
+ *      void* dest: start of the memory region to copy data onto
+ *
+ *      const void* src: start of the memory region to copy data from
+ *
+ *      size_t len: how many bytes to copy
+ *
+ * Output:
+ *      void*: a pointer to `dest`
+ *
+ * Note:
+ *      NOTE: I believe the return is redundant. A success value would be more appropriate
+ */
+void* myMemcpy (void* dest, const void* src, size_t len)
 {
-  char *d = dest;
-  const char *s = src;
+  char* d = dest;
+  const char* s = src;
   while (len--)
     *d++ = *s++;
   return dest;
 }
 
+/*
+ * This function set a `len` amount of bytes to `val` starting from `dest`
+ *
+ * Input:
+ *      void* dest: a pointer to the start of the memory region
+ *
+ *      int val: value to write
+ *
+ *      size_t len: amount of `val`s to write
+ *
+ * Output:
+ *      void*: a pointer tot `dest`
+ *
+ * Note:
+ *      NOTE: I believe the return is redundant. A success value would be more appropriate
+ */
 void* memset(void* dest, int val, size_t len)
 {
     unsigned char* ptr = dest;
@@ -65,6 +101,15 @@ void* memset(void* dest, int val, size_t len)
     return dest;
 }
 
+/*
+ * This function calculates the size of a CHAR excluding the null byte
+ *
+ * Input:
+ *      const CHAR* s1: string to calculate its size
+ *
+ * Output:
+ *      int: size of the string excluding the null byte
+ */
 int myStrlenA(const CHAR* s1)
 {
     const CHAR *s2 = s1; // Pointer to traverse the string
